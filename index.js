9181caff32e23887e65265d9f3f76cd1d98ebe7a
Nikola Ivanovic <nikola.ivanovic.ns@gmail.com>
2022-08-27 20:42:03 +0200

    Added dropdown on filter btn

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
 const filterBtn = document.querySelector(".filter-field__btn");
 const filterDropdown = document.querySelector(".filter-field__dropdown");
 
-console.log(document.querySelector(".filter-field__btn"));
-
 filterBtn.addEventListener("click", function () {
-  filterDropdown.classList.add("active");
+  filterDropdown.classList.toggle("active");
 });

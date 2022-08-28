1d96b991f41bc181ef454fd539853760dd23c3c7
Nikola Ivanovic <nikola.ivanovic.ns@gmail.com>
2022-08-28 22:01:31 +0200

    Added single country page with all dataa

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ document.querySelector("#search-form").addEventListener("submit", function (e) {
 
   fetch(`https://restcountries.com/v3.1/name/${countryName}`)
     .then((res) => res.json())
-    .then((data) => displayCountry(data));
+    .then((data) => displayAllCountries(data));
 });
 
 document.querySelector("#filter-form").addEventListener("submit", function (e) {
@@ -47,35 +47,127 @@ document.querySelector("#filter-form").addEventListener("submit", function (e) {
 
   fetch(`https://restcountries.com/v3.1/region/${region}`)
     .then((res) => res.json())
-    .then((data) => data.forEach((el) => displayCountry(el)));
+    .then((data) => displayAllCountries(data));
 });
 
-const displayCountry = function (data) {
-  console.log(data);
+const displayAllCountries = function (data) {
   const countriesContainer = document.querySelector(".countries-list");
-
-  const countryHtml = `<div class="country">
+  data.forEach((country) => {
+    const countryHtml = `<div class="country">
     <img
-      src="${data.flags.png}"
+      src="${country.flags.svg}"
       alt="flag"
       class="flag"
     />
     <div class="country__info">
-      <p class="country__info-name">${data.name.official}</p>
+      <p class="country__info-name">${country.name.common}</p>
       <p class="country__info-popultion">
         <span class="attr">Population:</span>
-        <span class="value">${data.population}</span>
+        <span class="value">${country.population}</span>
       </p>
       <p class="country__info-region">
         <span class="attr">Region:</span>
-        <span class="value">${data.region}</span>
+        <span class="value">${country.region}</span>
       </p>
       <p class="country__info-capital">
         <span class="attr">Capital:</span>
-        <span class="value">${data.capital}</span>
+        <span class="value">${country.capital}</span>
       </p>
     </div>
   </div>`;
 
-  countriesContainer.innerHTML += countryHtml;
+    countriesContainer.innerHTML += countryHtml;
+  });
+
+  document.querySelectorAll(".country").forEach((country) => {
+    country.addEventListener("click", function () {
+      displaySingleCountry(country);
+      addSingleCountry(
+        country.querySelector(".country__info-name").textContent
+      );
+    });
+  });
+};
+
+const displaySingleCountry = function (data) {
+  document.querySelector(".filtering-section").classList.add("hide");
+  document.querySelector(".countries-list").classList.add("hide");
+  document.querySelector(".single-country").classList.remove("hide");
+};
+
+const addSingleCountry = function (countryName) {
+  console.log("sdfnsdf");
+  fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    .then((res) => res.json())
+    .then((country) => {
+      country = country[0];
+      console.log(country);
+      const countryHtml = `<button class="back-btn">Back</button>
+      <div class="country">
+        <img
+          src="${country.flags.svg}"
+          alt="flag"
+          class="flag"
+        />
+        <div class="country__info">
+          <h1>${country.name.common}</h1>
+          <div class="country-details">
+            <div>
+              <p>
+                Native name: <span class="native-name">${
+                  country.name.official
+                }</span>
+              </p>
+              <p>Population: <span class="population">${
+                country.population
+              }</span></p>
+              <p>Region: <span class="region">${country.region}</span></p>
+              <p>Sub region: <span class="sub-region">${
+                country.subregion
+              }</span></p>
+              <p>Capital: <span class="capital">${country.capital}</span></p>
+            </div>
+            <div>
+              <p>
+                Top level domain: <span class="top-level-domain">${
+                  country.tld
+                }</span>
+              </p>
+              <p>Currencies: <span class="currencies">${currencies(
+                country.currencies
+              )}</span></p>
+              <p>Languages: <span class="languages">${languages(
+                country.languages
+              )}</span></p>
+            </div>
+          </div>
+          <div class="borders">
+            <p>
+              Border countries: <span>Sain</span><span>fance</span
+              ><span>gemany</span>
+            </p>
+          </div>
+        </div>
+      </div>`;
+
+      document.querySelector(".single-country").innerHTML = countryHtml;
+    });
+};
+
+const currencies = function (data) {
+  let str = "";
+  for (const [key, value] of Object.entries(data)) {
+    console.log(str);
+    str += `${value.name}, `;
+  }
+
+  return str.slice(0, -2);
+};
+
+const languages = function (data) {
+  let str = "";
+  for (const [key, value] of Object.entries(data)) {
+    str += `${value}, `;
+  }
+  return str.slice(0, -2);
 };

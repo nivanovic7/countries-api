761fe010b9b833fce14dbf027807c159ea4e3237
Nikola Ivanovic <nikola.ivanovic.ns@gmail.com>
2022-08-28 13:54:42 +0200

    form functionality added

--- index.js
+++ index.js
@@ -2,7 +2,33 @@
 
 const filterBtn = document.querySelector(".filter-field__btn");
 const filterDropdown = document.querySelector(".filter-field__dropdown");
+const searhInput = document.querySelector("input");
+const searchIcon = document.querySelector(".icon-search");
+const filterOptions = document.querySelectorAll(".filter-field__option");
 
 filterBtn.addEventListener("click", function () {
   filterDropdown.classList.toggle("active");
+});
+
+//SUBMITING FORM
+
+searhInput.addEventListener("submit", function () {
+  this.closest("form").requestSubmit();
+});
+
+searchIcon.addEventListener("click", function () {
+  this.closest("form").requestSubmit();
+});
+
+filterOptions.forEach((option) => {
+  option.addEventListener("click", function () {
+    this.closest("form").requestSubmit();
+    filterDropdown.classList.remove("active");
+    filterBtn.innerHTML = `${this.textContent} <span class="arrow"> &gt</span>`;
+    console.log(this.textContent);
+  });
+});
+
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
 });
